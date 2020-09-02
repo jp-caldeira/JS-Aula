2b4bd05d20eb3b060613a8597dd7d80dabde2bc4
jp-caldeira <jpcm86@gmail.com>
2020-09-02 20:29:42 -0300

    objetos js

--- teste.js
+++ teste.js
@@ -21,23 +21,58 @@ console.log("Último aluno é: " + ultimoAluno);
 
 //objetos
 
-var alunos2 = {};
+var aluno2 = {};
 
-var alunos2 = {
+var aluno2 = {
       "nome": "João",
       "idade": 33,
-      "aprovado": true
+      "nota": 5
 };
 
-console.log(alunos2);
+console.log(aluno2);
 
 //alterando objeto
-alunos2.nome = "Mendonça";
-alunos2.nota = 7;
+aluno2.nome = "Mendonça";
+aluno2.nota = 6;
 
-console.log(alunos2);
+console.log(aluno2);
 //outro jeito
 
-alunos2["idade"] = 34;
+aluno2["idade"] = 33;
 
-console.log(alunos2);
+aluno2["turma"] = "FullStack PHP";
+
+console.log(aluno2);
+
+//condicionais:
+if(aluno2.nota > 5){
+  console.log(aluno2.nome + ", parabéns! Você foi aprovado");
+} else {
+  console.log(aluno2.nome + ", sinto muito. Você foi reprovado.");
+}
+
+if (aluno2.nome == "Maria"){
+  console.log("Olá Maria");
+} else {
+  console.log("Quem é você?");
+}
+
+//if ternário - condicao ? true : false
+
+console.log(aluno2.idade >= 18 ? aluno2.nome + " é maior de idade" : aluno2.nome + " é menor de idade");
+
+//objetos
+
+var student = {
+    name: "Juana",
+    lastName: "Heinz",
+    fullName: function(){
+      return student.name + " " + student.lastName;
+    }
+}
+
+//console.log(student.fullName());
+
+for (var key in student){
+  console.log(student[key]);
+  }
